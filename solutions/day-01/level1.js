cc91a91882a7cf21e2a9ec4c17d2803548a1d0f3
Jackie <jackiele2000@gmail.com>
2022-07-25 11:32:43 -0500

    chore: exercise level1 1st commit test

---
+++ solutions/day-01/level1.js
@@ -0,0 +1,25 @@
+//1. 
+let array = [];
+//2 array of more than 5 items
+const animals = ['Cats', 'Dogs', 'horses', 'camels', 'pigs', 'cows']
+//3 find length of array
+console.log(animals.length)
+//4. Print first item, middle item, last item of array
+console.log(animals[0])
+console.log(animals.length - 1)
+console.log(animals[length / 2 ])
+
+//5 
+const mixedDataTypes = [1, 'poop', 3.2, 100.5, 'poopie', 100]
+console.log(mixedDataTypes.length)
+
+//6
+const itCompanies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+itCompanies.forEach((itCompanies, i) => {
+    console.log(itCompanies, i) 
+})
+console.log(itCompanies.length)
+
+itCompanies.forEach((itCompanies, i) => {
+    console.log(itCompanies.toUpperCase) 
+})
